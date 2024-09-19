e8aea6b8184b4535f93d780ad52929b6c8c8dc07
Lasse Hægland <seilenos@gmail.com>
2024-09-19 09:16:36 +0200

    Working

---
+++ tests/app.test.js
@@ -0,0 +1,21 @@
+import { add, subtract, multiply } from '../src/math';  // Using ES module syntax
+
+describe("simple arithmetic", () => {
+  describe("addition", () => {
+    test("expect 2 + 3 = 5", () => {
+      expect(add(2, 3)).toEqual(5);
+    });
+  });
+
+  describe("subtract", () => {
+    test("expect 5 - 2 = 3", () => {
+      expect(subtract(5, 2)).toEqual(3);
+    });
+  });
+
+  describe("multiply", () => {
+    test("expect 2 * 3 = 6", () => {
+      expect(multiply(2, 3)).toEqual(6);
+    });
+  });
+});
